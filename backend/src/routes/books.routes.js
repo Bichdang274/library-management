baa3f7c8128346218ab520aeec1bc891998dad59
DioBrandoHoHo <kocoface123@gmail.com>
2025-12-04 18:16:39 +0700

    Xoá toàn bộ dữ liệu trong nhánh Code/Huy

--- backend/src/routes/books.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getBooks, addBook } from "../controllers/books.controller.js";
-
-const router = Router();
-
-router.get("/", getBooks);
-router.post("/", addBook);
-
-export default router;
