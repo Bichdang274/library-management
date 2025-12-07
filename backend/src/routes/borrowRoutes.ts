96b4e141586e28ff0c5be5e6e76d49a5b31a7c27
Bich Dang <bichdang274@gmail.com>
2025-12-07 10:30:36 +0700

    b

--- backend/src/routes/borrowRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getBorrows } from "../controllers/borrowsController";
-
-const router: Router = Router();
-
-router.get("/", getBorrows);
-
-export default router;
--- backend/src/utils/helpers.ts
+++
@@ -1,4 +0,0 @@
-import type { Response } from 'express'; 
-exports.response = (res: Response, status: number, message: string, data: any = null) => {
-    return res.status(status).json({ message, data });
-};
